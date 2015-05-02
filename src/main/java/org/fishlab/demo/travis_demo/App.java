7e46384239fb53945258689cb0ddc0bda6d37933
Black Lotus <wu@fishlab.org>
2015-05-02 14:19:23 +0800

    w3

--- src/main/java/org/fishlab/demo/travis_demo/App.java
+++ src/main/java/org/fishlab/demo/travis_demo/App.java
@@ -8,6 +8,7 @@ public class App
         System.out.println( "Hello World!" );
         System.out.println("working branch!");
         System.out.println("working branch2");
+        System.out.println("working branch3");
 
         System.out.println("master branch1");
         System.out.println("master branch2");
