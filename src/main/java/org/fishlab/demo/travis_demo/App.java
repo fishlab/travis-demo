427cd7440a28952b286ea4d4cf3aa383cde3f780
Black Lotus <wu@fishlab.org>
2015-05-02 14:07:34 +0800

    hehe

--- src/main/java/org/fishlab/demo/travis_demo/App.java
+++ src/main/java/org/fishlab/demo/travis_demo/App.java
@@ -8,5 +8,6 @@ public class App
         System.out.println( "Hello World!" );
         System.out.println("working branch!");
         System.out.println("working branch2");
+        System.out.println("master branch1");
     }
 }
