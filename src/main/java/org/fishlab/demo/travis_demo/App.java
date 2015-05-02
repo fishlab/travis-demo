c365d6fa8a6ab27b0e64d2533625709892f5d7a4
Black Lotus <wu@fishlab.org>
2015-05-02 13:44:17 +0800

    test working branch

--- src/main/java/org/fishlab/demo/travis_demo/App.java
+++ src/main/java/org/fishlab/demo/travis_demo/App.java
@@ -1,13 +1,11 @@
 package org.fishlab.demo.travis_demo;
 
-/**
- * Hello world!
- *
- */
+
 public class App 
 {
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        System.out.println("working branch!");
     }
 }
